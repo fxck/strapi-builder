bb992008ba9b1472501b08f4de0b62fbc5d94593
Ales Rechtorik <foxxcz@gmail.com>
2020-04-30 14:43:59 +0200

    v

--- build/build.impl.ts
+++ build/build.impl.ts
@@ -2,14 +2,9 @@ import { BuilderOutput, createBuilder } from '@angular-devkit/architect';
 import { spawn } from 'child_process';
 
 
-// alias spawn
-const exec = (commands: any) => {
-  return spawn(commands, { stdio: "inherit", shell: true });
-};
-
 export default createBuilder((options, context) => {
   return new Promise<BuilderOutput>(resolve => {
-    const ls = exec('strapi build');
+    const ls = spawn('strapi', ['-v']);
 
     ls.on('close', (code) => {
       console.log(`Exited with code ${code}`);
