35a0c9ca6549a6c2414d22087aa0b0423c3d4499
Ales Rechtorik <foxxcz@gmail.com>
2020-04-30 14:28:38 +0200

    v

--- build/build.impl.ts
+++ build/build.impl.ts
@@ -3,15 +3,25 @@ import { spawn } from 'child_process';
 
 export default createBuilder((options, context) => {
   return new Promise<BuilderOutput>(resolve => {
-    const ls = spawn('strapi build');
+    const ls = spawn('strapi', ['build']);
 
     ls.on('close', (code) => {
       console.log(`Exited with code ${code}`);
       resolve({ success: true });
     });
 
+    ls.stderr.on('data', (data) => {
+      console.log(`stderr: ${data}`);
+    });
+
+    ls.on('error', (error) => {
+      console.log(`error: ${error.message}`);
+    });
+
     ls.stdout.on('data', (data) => {
       console.log(`stdout: ${data}`);
     });
+
   });
+
 });
