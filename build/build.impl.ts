4c6d042229e7f27474e1bab957e3d67a9fd7f5c3
Ales Rechtorik <foxxcz@gmail.com>
2020-04-30 15:04:33 +0200

    v

--- build/build.impl.ts
+++ build/build.impl.ts
@@ -3,10 +3,11 @@ import { spawn } from 'child_process';
 
 export default createBuilder((options, context) => {
   return new Promise<BuilderOutput>(resolve => {
+    console.log(options, context);
     const ls = spawn('./node_modules/.bin/strapi', [ 'build' ]);
 
     ls.on('close', (code) => {
-      console.log(`Exited with code pica ${code}`);
+      console.log(`Exited with code ${code}`);
       resolve({ success: true });
     });
 
