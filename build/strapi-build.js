9cc9b296f3119ecc2756b32b118f6444f328a546
Ales Rechtorik <foxxcz@gmail.com>
2020-05-15 13:55:50 +0200

    0.1.13

--- build/strapi-build.js
+++ build/strapi-build.js
@@ -155,6 +155,8 @@
 const { green } = require('chalk');
 // eslint-disable-next-line node/no-extraneous-require
 const strapiAdmin = require('strapi-admin');
+const fs = require('fs-extra');
+const yargs = require('yargs');
 const loadConfiguration = require('../core/app-configuration');
 const addSlash = require('../utils/addSlash');
 /**
@@ -166,7 +168,6 @@ const buildCommand = async ({
   dir = process.cwd(),
   outputPath
 }) => {
-  const dir = process.cwd();
   const config = loadConfiguration(dir);
 
   const adminPath = config.get('server.admin.path', '/admin');
